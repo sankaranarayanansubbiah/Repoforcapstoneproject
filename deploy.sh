d4dce650518e18bc4494287a6f2580b9ed007485
sankaranarayanan <163542734+sankaranarayanansubbiah@users.noreply.github.com>
2024-06-26 21:59:37 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Retrieve the current Git branch name
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+# Debugging output
 echo "Current Git Branch: ${GIT_BRANCH}"
 
+# Check which branch is currently checked out
 if [[ "${GIT_BRANCH}" == "main" ]]; then
     ./build.sh
     docker tag guvi sankaranarayanansubbiah/prod:latest
@@ -12,5 +16,6 @@ elif [[ "${GIT_BRANCH}" == "dev" ]]; then
     docker tag guvi sankaranarayanansubbiah/dev:latest
     docker push sankaranarayanansubbiah/dev:latest
 else
-    echo "deployment failed"
+    echo "Deployment failed: Branch ${GIT_BRANCH} is not supported for deployment."
 fi
+
