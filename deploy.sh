5f35ee1b05e8ee36cbe1682283440a7f8abe8a71
sankaranarayanan <163542734+sankaranarayanansubbiah@users.noreply.github.com>
2024-06-26 22:43:33 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,11 @@ GIT_BRANCH=${GIT_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
 echo "Current Git Branch: ${GIT_BRANCH}"
 
 # Check which branch is currently checked out
-if [[ "${GIT_BRANCH}" == "main" ]]; then
+if [[ "${GIT_BRANCH}" == "origin/main" ]]; then
     ./build.sh
     docker tag guvi sankaranarayanansubbiah/prod:latest
     docker push sankaranarayanansubbiah/prod:latest
-elif [[ "${GIT_BRANCH}" == "dev" ]]; then
+elif [[ "${GIT_BRANCH}" == "origin/dev" ]]; then
     ./build.sh
     docker tag guvi sankaranarayanansubbiah/dev:latest
     docker push sankaranarayanansubbiah/dev:latest
